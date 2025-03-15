fc2e178b0b00bd84eeb3a31da83782727a125205
JinGoo <zzgg3040@naver.com>
2025-03-15 11:42:33 +0900

    feat:cat-controller-and-service

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,34 @@
+import { CatsService } from './cats.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Get()
+  findAll() {
+    return this.catsService.findAll();
+  }
+
+  @Post()
+  createOne(@Body() createCatDto: CreateCatDto) {
+    console.log(`created ${createCatDto.name}, ${createCatDto.age}`);
+    this.catsService.create(createCatDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return `cat ${id}`;
+  }
+
+  //   @Patch(':id')
+  //   updateOne(@Param('id', ParseIntPipe) id: number, @Body)
+}
